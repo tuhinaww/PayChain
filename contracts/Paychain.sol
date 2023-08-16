446fdf4c794f4e053e23f1d51c214878faaaf93f
tuhinaww <tuhina_tripathi@hotmail.com>
2023-08-17 00:59:00 +0530

    created struct for transaction

--- contracts/Paychain.sol
+++ contracts/Paychain.sol
@@ -10,7 +10,27 @@ contract Paychain{
          //called on once
          //check - deployer of smart contract
 }
-
+// create Struct and Mapping for request, transaction and name
+struct request {
+    address requestor;
+    uint256 amount;
+    string message;
+    string name;
+}
+struct sendRecieve {
+    string action;
+    uint256 amount;
+    string message;
+    address otherPartyAddress;
+    string otherParttName;
+}
+struct userName{
+    string name;
+    bool hasName;
+}
+mapping(address => userName) names;
+mapping(address => request[]) requests;
+mapping(address => sendRecieve[]) history;
 
 
 }
