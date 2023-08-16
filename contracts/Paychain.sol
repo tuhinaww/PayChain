49f9713faeda1124464f7cb887674cf694039ef5
tuhinaww <tuhina_tripathi@hotmail.com>
2023-08-17 00:51:31 +0530

    Owner defined

--- contracts/Paychain.sol
+++ contracts/Paychain.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.9;
 
 contract Paychain{
-    
+// owner of smart contract
+   address public owner;
+
+   constructor(){
+         owner = msg.sender;
+         //called on once
+         //check - deployer of smart contract
+}
+
+
+
 }
