c88a2ac4f6815e1e952b6c327df78752b18519de
tuhinaww <tuhina_tripathi@hotmail.com>
2023-08-19 17:19:38 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,16 @@ app.use(express.json());
 
 app.get("/getNameAndBalance", async (req, res) => {
 
+ const { userAddress } = req.query;
 
+ const response = await payc.EvmApi.utils.runContractFunction({
+    chain: "0x13881",
+    address: "0xFB7C1A4a51Fc414c37fD1042653486fd34B9917A",
+    abi: ABI,
+    functionName: "getMyName",
+    params: { _user: userAddress },
+
+ });
 
   return res.status(200).json({});
 });
