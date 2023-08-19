21aaa8c15c41b81bdd54aef4a23d35510bab8fec
tuhinaww <tuhina_tripathi@hotmail.com>
2023-08-19 17:02:03 +0530

    modified:   frontend/paychain/src/index.css 	modified:   frontend/paychain/src/index.js

--- frontend/paychain/src/index.js
+++ frontend/paychain/src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureChains, mainnet, WagmiConfig, createClient } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { polygonMumbai } from '@wagmi/chains';
+
+const { provider, webSocketProvider } = configureChains(
+  [mainnet, polygonMumbai],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={client}>
+        <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
