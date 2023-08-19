21b3ebf10855ac37358d2232da28c9db4bad857f
tuhinaww <tuhina_tripathi@hotmail.com>
2023-08-19 17:48:48 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const payc = require("payc").default;
+const Moralis = require("moralis").default;
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -9,27 +9,84 @@ const ABI = require("./abi.json");
 app.use(cors());
 app.use(express.json());
 
+function convertArrayToObjects(arr) {
+  const dataArray = arr.map((transaction, index) => ({
+    key: (arr.length + 1 - index).toString(),
+    type: transaction[0],
+    amount: transaction[1],
+    message: transaction[2],
+    address: `${transaction[3].slice(0,4)}...${transaction[3].slice(0,4)}`,
+    subject: transaction[4],
+  }));
+
+  return dataArray.reverse();
+}
 
 app.get("/getNameAndBalance", async (req, res) => {
+  const { userAddress } = req.query;
 
- const { userAddress } = req.query;
-
- const response = await payc.EvmApi.utils.runContractFunction({
+  const response = await Moralis.EvmApi.utils.runContractFunction({
     chain: "0x13881",
     address: "0xFB7C1A4a51Fc414c37fD1042653486fd34B9917A",
-    abi: ABI,
     functionName: "getMyName",
+    abi: ABI,
     params: { _user: userAddress },
+  });
 
- });
+  const jsonResponseName = response.raw;
 
-  return res.status(200).json({});
+  const secResponse = await Moralis.EvmApi.balance.getNativeBalance({
+    chain: "0x13881",
+    address: userAddress,
+  });
+
+  const jsonResponseBal = (secResponse.raw.balance / 1e18).toFixed(2);
+
+  const thirResponse = await Moralis.EvmApi.token.getTokenPrice({
+    address: "0x9199dEF6ECC78ff7bdCC98E10616bdCA7debc801",
+  });
+
+  const jsonResponseDollars = (
+    thirResponse.raw.usdPrice * jsonResponseBal
+  ).toFixed(2);
+
+  const fourResponse = await Moralis.EvmApi.utils.runContractFunction({
+    chain: "0x13881",
+    address: "0xFB7C1A4a51Fc414c37fD1042653486fd34B9917A",
+    functionName: "getMyHistory",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseHistory = convertArrayToObjects(fourResponse.raw);
+
+
+  const fiveResponse = await Moralis.EvmApi.utils.runContractFunction({
+    chain: "0x13881",
+    address: "0xFB7C1A4a51Fc414c37fD1042653486fd34B9917A",
+    functionName: "getMyRequests",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseRequests = fiveResponse.raw;
+
+
+  const jsonResponse = {
+    name: jsonResponseName,
+    balance: jsonResponseBal,
+    dollars: jsonResponseDollars,
+    history: jsonResponseHistory,
+    requests: jsonResponseRequests,
+  };
+
+  return res.status(200).json(jsonResponse);
 });
 
 
 
-payc.start({
-  apiKey: process.env.PAY_KEY,
+Moralis.start({
+  apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
     console.log(`Listening for API Calls`);
