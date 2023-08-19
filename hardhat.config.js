850d70fb375d40c3031dc20c85442755d1649f1c
tuhinaww <tuhina_tripathi@hotmail.com>
2023-08-19 09:20:44 +0530

    modified: hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-waffle");
+require("@nomicfoundation/hardhat-etherscan");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
+
 };
